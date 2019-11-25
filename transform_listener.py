5e5c57494593466808013aefe854fbe953417938
cmletherbarrow <cmletherbarrow@wpi.edu>
2019-11-25 15:05:56 -0500

    Update to get back to master

--- transform_listener.py
+++ transform_listener.py
@@ -4,7 +4,7 @@ import rospy
 import math
 import tf
 import geometry_msgs.msg
-import turtlesim.srv
+
 
 if __name__ == '__main__':
     rospy.init_node('tf_listener')
@@ -14,16 +14,12 @@ if __name__ == '__main__':
     rate = rospy.Rate(10.0)
     while not rospy.is_shutdown():
         try:
-            (trans,rot) = listener.lookupTransform('/base_link', '/map', rospy.Time(0))
+            (trans,rot) = listener.lookupTransform('/odom', '/map', rospy.Time(0))
         except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
             continue
         #trans = list of x,y,z translations
         #rot = list of x,y,z,w rotations
-        angular = 4 * math.atan2(trans[1], trans[0])
-        linear = 0.5 * math.sqrt(trans[0] ** 2 + trans[1] ** 2)
-        cmd = geometry_msgs.msg.Twist()
-        cmd.linear.x = linear
-        cmd.angular.z = angular
-        turtle_vel.publish(cmd)
-
+        print(trans)
+        print("X Position" + trans[0])
+        print("Y Position" + trans[1])
         rate.sleep()
