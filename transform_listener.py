632b380e08a6942a323f0054206d802cff2df5d2
cmletherbarrow <cmletherbarrow@wpi.edu>
2019-12-11 22:34:22 -0500

    Added in comments to tf listener

--- transform_listener.py
+++ transform_listener.py
@@ -1,30 +1,24 @@
 #!/usr/bin/env python
 import roslib
 import rospy
-import math
 import tf
-import geometry_msgs.msg
-# import transform.msg
-from nav_msgs.msg import Odometry
-from sensor_msgs.msg import Imu, LaserScan
-from tf.transformations import euler_from_quaternion
-from pathplanningmqp.msg import transform
+from pathplanningmqp.msg import transform # custom transform message type is a 32bit unsigned int, used to store a single position value
 
 if __name__ == '__main__':
     rospy.init_node('tf_listener')
 
-    listener = tf.TransformListener()
-    trans_pub = rospy.Publisher('/linear_trans', transform, queue_size = 2)
+    listener = tf.TransformListener() # Create transform listener to obtain map pose estimate
+    trans_pub = rospy.Publisher('/linear_trans', transform, queue_size = 2) # Create publisher along linear_trans topic with custom transform message type
     listener.waitForTransform('/odom','/map',rospy.Time(), rospy.Duration(4.0))
-    rate = rospy.Rate(10.0)
+    rate = rospy.Rate(10.0) # rate at which the tf publisher updates
     while not rospy.is_shutdown():
         try:
-            listener.waitForTransform('/odom','/map',rospy.Time(0),rospy.Duration(4.0))
-            (trans,rot) = listener.lookupTransform('/odom', '/map', rospy.Time(0))
+            listener.waitForTransform('/odom','/map',rospy.Time(0),rospy.Duration(4.0)) # Waits for a transform to be recieved, then moves onto next step. Will not progress unless tf is recieved
+            (trans,rot) = listener.lookupTransform('/odom', '/map', rospy.Time(0)) # trans is the X, Y, and Z linear transform vector, while rot is the rho, theta, phi rotational transform vector
         except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
             continue
         print(trans)
-        print("X Position: " + str(trans[0] * -1))
-        print("Y Position: " + str(trans[1] * -1))
-        trans_pub.publish(trans[1]*-1)
+        print("X Position: " + str(trans[0] * -1)) # TF reports the position as inverted (negative when should be positive), so swap polarity
+        print("Y Position: " + str(trans[1] * -1)) # TF reports the position as inverted (negative when should be positive), so swap polarity
+        trans_pub.publish(trans[1]*-1) # Publish the Y transform, which is the estimated Y position of the robot
         rate.sleep()
